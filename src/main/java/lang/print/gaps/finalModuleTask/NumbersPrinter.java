e3c10771c18ec72b2a30bf6027472cd26f46ce87
VitaliyBaranovskyy <40470956+VitaliyBaranovskyy@users.noreply.github.com>
2023-02-19 22:03:05 +0200

    Update NumbersPrinter.java

--- src/main/java/lang/print/gaps/finalModuleTask/NumbersPrinter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumbersPrinter.java
@@ -2,6 +2,14 @@ package lang.print.gaps.finalModuleTask;
 
 public class NumbersPrinter {
     public void printIsPositive(int number) {
+        
+        System.out.println(number>=0);
 
+    }
+    
+    public static void main(String[] args) {
+        NumbersPrinter main = new NumbersPrinter() ;
+        main.printIsPositive(5);
+        
     }
 }
