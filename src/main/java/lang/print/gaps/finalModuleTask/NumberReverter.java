e45ac905ed5d98b83c09a5b56b9abbe7090cecdc
VitaliyBaranovskyy <40470956+VitaliyBaranovskyy@users.noreply.github.com>
2023-02-19 21:57:28 +0200

    Update NumberReverter.java

--- src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
@@ -2,5 +2,14 @@ package lang.print.gaps.finalModuleTask;
 
 public class NumberReverter {
     public void revert(int number) {
+        
+        int res = number * 2 + 6 ;
+        System.out.println(res);
     }
+    
+    public static void main(String[] args) {
+        NumberReverter main = new NumberReverter();
+        main.revert(489);
+        
+    }  
 }
